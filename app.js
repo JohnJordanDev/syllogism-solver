3673a8ebea91517ea394bbbe99bebe8c30060337
John Jordan <johnjordan1985@gmail.com>
2020-12-03 08:59:58 -0800

    refactor app.js and user-interface.js post linter and add functional sections to app.js for clarity

--- app.js
+++ app.js
@@ -1,38 +1,8 @@
 window.app = (function appInit() {
+  // ====== app - state ======
+
   let middleTermIsPredicateOfMajor = false;
   let middleTermIsSubjectOfMinor = false;
-  const validMoodsFirstFigure = {
-    AA: 'A',
-    EA: 'E',
-    AI: 'I',
-    EI: 'O',
-  };
-  const invalidMoodsFirstFigure = {
-    AE: ['A', 'E', 'I', 'O'],
-    AO: ['I', 'O'],
-    EE: [],
-    EO: [],
-    IA: [],
-    IE: [],
-    II: [],
-    IO: [],
-    OA: [],
-    OE: [],
-    OI: [],
-    OO: [],
-  };
-  const formOfPropositions = {
-    allare: 'A',
-    noare: 'E',
-    someare: 'I',
-    somearenot: 'O',
-  };
-  const validConclusions = {
-    A: "all 'B' are 'A'",
-    E: "no 'B' are 'A'",
-    I: "some 'B' are 'A'",
-    O: "some 'B' are NOT 'A'",
-  };
   const isMiddleTermPredicateOfMajor = () => middleTermIsPredicateOfMajor;
 
   const isMiddleTermSubjectOfMinor = () => middleTermIsSubjectOfMinor;
@@ -45,29 +15,77 @@ window.app = (function appInit() {
     middleTermIsSubjectOfMinor = Boolean(value);
   };
 
-  const getFormOfPropositions = (prop1Quantity, prop1Quality, prop2Quantity, prop2Quality) => {
+  // ====== utils - UI ======
+
+  const validMoodsFirstFigure = {
+    AA: "A",
+    EA: "E",
+    AI: "I",
+    EI: "O",
+  };
+  // possible conclusions to draw, given invalid moods
+  const invalidMoodsFirstFigure = {
+    AE: ["A", "E", "I", "O"],
+    AO: ["I", "O"],
+    EE: [],
+    EO: [],
+    IA: [],
+    IE: [],
+    II: [],
+    IO: [],
+    OA: [],
+    OE: [],
+    OI: [],
+    OO: [],
+  };
+  const formOfPropositions = {
+    allare: "A",
+    noare: "E",
+    someare: "I",
+    somearenot: "O",
+  };
+  const validConclusions = {
+    A: "all 'B' are 'A'",
+    E: "no 'B' are 'A'",
+    I: "some 'B' are 'A'",
+    O: "some 'B' are NOT 'A'",
+  };
+
+  // ====== utils - app logic ======
+
+  const getFormOfPropositions = (
+    prop1Quantity,
+    prop1Quality,
+    prop2Quantity,
+    prop2Quality
+  ) => {
     const prop1Form = formOfPropositions[prop1Quantity + prop1Quality];
     const prop2Form = formOfPropositions[prop2Quantity + prop2Quality];
     return prop1Form + prop2Form;
   };
   const getConclusion = (formOfBothPropositions) => {
     const conclusion = validMoodsFirstFigure[formOfBothPropositions];
-    if (typeof conclusion !== 'undefined' && typeof validConclusions[conclusion] !== 'undefined') {
+    if (
+      typeof conclusion !== "undefined" &&
+      typeof validConclusions[conclusion] !== "undefined"
+    ) {
       return validConclusions[conclusion];
     }
     return null;
   };
 
+  // ====== utils - UI ======
+
   const hideAreNotOptionOfPropQuality = (propQualityId) => {
     const propQualityElem = window.document.getElementById(propQualityId);
-    propQualityElem.value = 'are';
-    propQualityElem.querySelectorAll('option').forEach((optionElem) => {
-      if (optionElem.value === 'arenot') {
-        optionElem.setAttribute('disabled', 'disabled');
-        optionElem.removeAttribute('selected');
+    propQualityElem.value = "are";
+    propQualityElem.querySelectorAll("option").forEach((optionElem) => {
+      if (optionElem.value === "arenot") {
+        optionElem.setAttribute("disabled", "disabled");
+        optionElem.removeAttribute("selected");
       }
-      if (optionElem.value === 'are') {
-        optionElem.setAttribute('selected', 'true');
+      if (optionElem.value === "are") {
+        optionElem.setAttribute("selected", "true");
       }
     });
   };
@@ -75,16 +93,16 @@ window.app = (function appInit() {
   // Only valid quantity given quality of 'are NOT', is 'some'
   const selectSomeOptionOfPropQuantity = (propQuantityId) => {
     const propQuantityElem = window.document.getElementById(propQuantityId);
-    propQuantityElem.value = 'some';
+    propQuantityElem.value = "some";
   };
 
   // showAreNotOptionOfPropQuality(), to undo effect of hideAreNotOptionOfPropQuality
   const showAreNotOptionOfPropQuality = (propQualityId) => {
     const propQualityElem = window.document.getElementById(propQualityId);
-    propQualityElem.querySelectorAll('option').forEach((optionElem) => {
+    propQualityElem.querySelectorAll("option").forEach((optionElem) => {
       // Want to maintain disabled status of 'placeholder' element
-      if (optionElem.value === 'arenot') {
-        optionElem.removeAttribute('disabled');
+      if (optionElem.value === "arenot") {
+        optionElem.removeAttribute("disabled");
       }
     });
   };
@@ -100,4 +118,4 @@ window.app = (function appInit() {
     setValueOfIsMiddleTermPredicateOfMajor,
     setValueOfIsMiddleTermSubjectOfMinor,
   };
-}());
+})();
--- user-interface.js
+++ user-interface.js
@@ -1,76 +1,85 @@
 // ToDo: Place all this on an object
-var prop1Quantity = window.document.getElementById('prop_one_quantity');
-var prop1Quality = window.document.getElementById('prop_one_quality');
-var prop2Quantity = window.document.getElementById('prop_two_quantity');
-var prop2Quality = window.document.getElementById('prop_two_quality'); 
+const prop1Quantity = window.document.getElementById("prop_one_quantity");
+const prop1Quality = window.document.getElementById("prop_one_quality");
+const prop2Quantity = window.document.getElementById("prop_two_quantity");
+const prop2Quality = window.document.getElementById("prop_two_quality");
 
-var canvasPropOne = window.document.getElementById('canvas_prop_one');
-var canvasPropOneCtx = canvasPropOne.getContext('2d');
+const canvasPropOne = window.document.getElementById("canvas_prop_one");
+const canvasPropOneCtx = canvasPropOne.getContext("2d");
 
-var canvasPropTwo = window.document.getElementById('canvas_prop_two');
-var canvasPropTwoCtx = canvasPropTwo.getContext('2d');
+const canvasPropTwo = window.document.getElementById("canvas_prop_two");
+const canvasPropTwoCtx = canvasPropTwo.getContext("2d");
 
-var canvasConclusion = window.document.getElementById('canvas_conclusion');
-var canvasConclusionCtx = canvasConclusion.getContext('2d');
+const canvasConclusion = window.document.getElementById("canvas_conclusion");
+const canvasConclusionCtx = canvasConclusion.getContext("2d");
 
-const firstFigure = window.document.getElementById('first_figure');
-const firstFigureSubmit = window.document.getElementById('first_figure_submit');
-const conclusionOutputElem = window.document.getElementById('conclusion');
+const firstFigure = window.document.getElementById("first_figure");
+const firstFigureSubmit = window.document.getElementById("first_figure_submit");
+const conclusionOutputElem = window.document.getElementById("conclusion");
 
 const drawPremisesAndConclusion = () => {
-    let formsOfPropositions = window.app.getFormOfPropositions(
-        prop1Quantity.value, prop1Quality.value, prop2Quantity.value, prop2Quality.value
-    );
-    let conclusionContent =  window.app.getConclusion(formsOfPropositions); 
-    app.canvas.drawPremise('major', formsOfPropositions[0], canvasPropOneCtx);
-    app.canvas.drawPremise('minor', formsOfPropositions[1], canvasPropTwoCtx);
-    if(conclusionContent) {
-        debugger;
-        app.canvas.drawConclusion(formsOfPropositions, canvasConclusionCtx);
-        conclusionOutputElem.innerHTML = 'Then, ' + conclusionContent;
-    } else {
-        conclusionOutputElem.innerHTML = 'Then, we cannot draw a valid conclusion';
-    }
+  const formsOfPropositions = window.app.getFormOfPropositions(
+    prop1Quantity.value,
+    prop1Quality.value,
+    prop2Quantity.value,
+    prop2Quality.value
+  );
+  const conclusionContent = window.app.getConclusion(formsOfPropositions);
+  app.canvas.drawPremise("major", formsOfPropositions[0], canvasPropOneCtx);
+  app.canvas.drawPremise("minor", formsOfPropositions[1], canvasPropTwoCtx);
+  if (conclusionContent) {
+    debugger;
+    app.canvas.drawConclusion(formsOfPropositions, canvasConclusionCtx);
+    conclusionOutputElem.innerHTML = `Then, ${conclusionContent}`;
+  } else {
+    conclusionOutputElem.innerHTML = "Then, we cannot draw a valid conclusion";
+  }
 };
 
 const triggerFormUiFeedback = () => {
-    firstFigureSubmit.click();
+  firstFigureSubmit.click();
 };
 
 const updateFormOutputs = () => {
-    app.canvas.clearAllCanvasses();
-    if(firstFigure.checkValidity()) {
-        drawPremisesAndConclusion();
+  app.canvas.clearAllCanvasses();
+  if (firstFigure.checkValidity()) {
+    drawPremisesAndConclusion();
+  } else {
+    triggerFormUiFeedback();
+    conclusion.innerHTML = "...";
+  }
+};
+
+const setValidUserChoicesBasedOnInput = (event) => {
+  const elem = event.target;
+  const elemId = elem.getAttribute("id");
+  if (elemId === "prop_one_quantity" || elemId === "prop_two_quantity") {
+    if (elem.value === "no" || elem.value === "all") {
+      window.app.hideAreNotOptionOfPropQuality(
+        elemId.replace("quantity", "quality")
+      );
     } else {
-        triggerFormUiFeedback(); 
-        conclusion.innerHTML = '...';  
+      window.app.showAreNotOptionOfPropQuality(
+        elemId.replace("quantity", "quality")
+      );
     }
-};
-
-const setValidUserChoicesBasedOnInput = event => {
-    var elem = event.target;
-    var elemId = elem.getAttribute('id');
-    if(elemId === 'prop_one_quantity' || elemId === 'prop_two_quantity') {
-        if(elem.value === 'no' || elem.value === 'all'){
-            window.app.hideAreNotOptionOfPropQuality(elemId.replace('quantity', 'quality'));     
-        } else {
-            window.app.showAreNotOptionOfPropQuality(elemId.replace('quantity', 'quality'));
-        }
-    } else if(elemId === 'prop_one_quality' || elemId === 'prop_two_quality') {
-        if(elem.value === 'arenot'){
-            window.app.selectSomeOptionOfPropQuantity(elemId.replace('quality', 'quantity'));     
-        } 
+  } else if (elemId === "prop_one_quality" || elemId === "prop_two_quality") {
+    if (elem.value === "arenot") {
+      window.app.selectSomeOptionOfPropQuantity(
+        elemId.replace("quality", "quantity")
+      );
     }
+  }
 };
 
-const changeHandler = event => {
-    setValidUserChoicesBasedOnInput(event);
-    updateFormOutputs();
+const changeHandler = (event) => {
+  setValidUserChoicesBasedOnInput(event);
+  updateFormOutputs();
 };
 
-window.document.addEventListener('change', changeHandler);
+window.document.addEventListener("change", changeHandler);
 
-window.document.getElementById('first_figure').reset();
+window.document.getElementById("first_figure").reset();
 
 // For testing purposes
 prop1Quantity.selectedIndex = 1;
@@ -78,7 +87,7 @@ prop1Quality.selectedIndex = 1;
 prop2Quantity.selectedIndex = 2;
 prop2Quality.selectedIndex = 1;
 
-var changeEvent = new Event('change', {bubbles: true});
+const changeEvent = new Event("change", { bubbles: true });
 
 // Dispatch it.
 prop1Quantity.dispatchEvent(changeEvent);
