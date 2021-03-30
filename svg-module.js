e1dee36217e3a117e197f1e4c5d30a4586192f74
John Jordan <johnjordan1985@gmail.com>
2021-03-30 08:18:56 -0700

    refactor: svg styles into main CSS, text and circle generation into new functions

--- svg-module.js
+++ svg-module.js
@@ -1,8 +1,12 @@
 const svgModule = (function () {
+  // See here for https://stackoverflow.com/questions/15500894/background-color-of-text-in-svg text box
+  const getTextLabel = () => '<text text-anchor="middle" x="50%" y="20">Foo</text>';
+
+  const getCircleShape = () => `<circle cx="50%" cy="50%" r="50"/>`;
+
   const drawCircle = (svgElem) => {
     // eslint-disable-next-line no-param-reassign
-    svgElem.innerHTML
-      += '<circle cx="50%" cy="50%" r="50" style="fill: blue; stroke: black; stroke-width: 2px;"/>';
+    svgElem.innerHTML += `${getTextLabel()}${getCircleShape()}`;
   };
 
   return {
