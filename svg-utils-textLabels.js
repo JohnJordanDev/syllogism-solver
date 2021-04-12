ca165ed8cdc8e9216563bb766ff9cf7d87657728
John Jordan <johnjordan1985@gmail.com>
2021-04-12 07:32:56 -0700

    feat: create local store for data label differences

--- svg-utils-textLabels.js
+++ svg-utils-textLabels.js
@@ -1,15 +1,41 @@
 const utilsSVGTextLabels = (function (SVGStore) {
+  const storeOfTextLabelData = {
+    majorTerm: {
+      textAnchor: "start"
+    },
+    middleTerm: {
+      textAnchor: "middle"
+    },
+    minorTerm: {
+      textAnchor: "end"
+    }
+  };
+
+  const getLabelYPos = (inputElementIdentifier, storeSettings) => (
+    inputElementIdentifier === "minorTerm"
+      ? storeSettings.circleYPos + storeSettings.circleRadius + 10
+      : storeSettings.circleYPos - storeSettings.circleRadius
+  );
+
+  const getTextLabelSpecificSettings = (inputElementIdentifier, storeSettings) => {
+    const labelYPos = getLabelYPos(inputElementIdentifier, storeSettings);
+    return { ...storeSettings, ...storeOfTextLabelData[inputElementIdentifier], labelYPos };
+  };
+
   const updateAttributes = (element, attributes) => {
     Object.keys(attributes).forEach((attribute) => {
       element.setAttribute(attribute, attributes[attribute]);
     });
   };
+
   // TODO; Background image See here for https://stackoverflow.com/questions/15500894/background-color-of-text-in-svg text box
+
   const textElemFromInputElemData = (text, inputElemIdentififer, labelSettings) => {
     const nsSVG = "http://www.w3.org/2000/svg";
     const t = window.document.createElementNS(nsSVG, "text");
+    console.log("getTextLabelSpecificSettings: ", getTextLabelSpecificSettings(inputElemIdentififer, labelSettings));
     updateAttributes(t, {
-      "text-anchor": "end",
+      "text-anchor": labelSettings.textAnchor,
       x: labelSettings.circleXPos,
       y: labelSettings.labelYPos,
       "data-identifier": `textLabel-${inputElemIdentififer}`
@@ -21,12 +47,13 @@ const utilsSVGTextLabels = (function (SVGStore) {
 
   const createSVGTextLabel = (inputElem, labelSettings) => {
     const inputElemIdentififer = inputElem.attributes.id.value.split("_")[0];
+    const textLabelSpecificSettings = getTextLabelSpecificSettings(inputElemIdentififer, labelSettings);
     window.document
       .querySelectorAll(`[data-identifier*="${inputElemIdentififer}"]`)
       .forEach((shape) => {
         shape.classList.add("changing");
       });
-    return textElemFromInputElemData(inputElem.value, inputElemIdentififer, labelSettings);
+    return textElemFromInputElemData(inputElem.value, inputElemIdentififer, textLabelSpecificSettings);
   };
 
   const addNewTextLabelBesideTarget = (inputElem, targetShape, labelSettings) => {
@@ -91,23 +118,16 @@ const utilsSVGTextLabels = (function (SVGStore) {
     );
 
     listOfSVGShapeTargets.forEach((targetShape) => {
-      // need part of syllogism
       const partOfSyllogism = targetShape.parentNode.attributes.id.value.split(
         "_"
       )[1];
-      // need subject or predicate
       const subjectOrPredicate = determineSubjectOrPredicate(
         inputElemTermIdentififer,
         partOfSyllogism
       );
       const formOfPartOfSyllogism = getFormOfPartOfSyllogism(partOfSyllogism, formsOfPropositions, conclusionForm);
       const settingsFromStore = SVGStore.getCircleShape(partOfSyllogism, subjectOrPredicate, formOfPartOfSyllogism);
-      const textLabelSettings = { ...settingsFromStore, labelYPos: settingsFromStore.circleYPos - settingsFromStore.circleRadius};
-      console.log(settingsFromStore, textLabelSettings);
-      // TODO function to create new yPos from settings reflecting offset, for the label yPos
-      // which will be used to position textLabel, and the original circleYPos used for line marker (to come)
-
-      // need formOfProsition
+      const textLabelSettings = { ...settingsFromStore, labelYPos: settingsFromStore.circleYPos - settingsFromStore.circleRadius };
       updateExistingOrAddNewTextLabel(inputElem, targetShape, textLabelSettings);
     });
   };
