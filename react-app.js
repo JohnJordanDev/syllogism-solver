c5d32c811f4f6c1b2d784aa8880abd0083bd602c
John Jordan <johnjordan1985@gmail.com>
2021-07-20 20:54:48 -0700

    refactor: use hidden attribute rather than onBlur/onFocus handling of placeholder text in select elements

--- react-app.js
+++ react-app.js
@@ -7,14 +7,81 @@
 
 const { React, ReactDOM, PropTypes } = window;
 
-const Part = (props) => {
+const Quality = (props) => {
+	const { value } = props;
+	return (
+  <>
+    <label htmlFor="prop_one_quality">Quality</label>
+    <select id="prop_one_quality" className="" required value={value}>
+      <option value="none" disabled hidden>are/are NOT</option>
+      <option value="are">are</option>
+      <option value="arenot">are NOT</option>
+    </select>
+  </>
+	);
+};
+
+Quality.propTypes = {
+	value: PropTypes.string.isRequired
+};
+
+const Quantity = (props) => {
+	const { value } = props;
+	return (
+  <>
+    <label htmlFor="prop_one_quantity">Quantity</label>
+    <select
+      id="prop_one_quantity"
+      className=""
+      required
+      value={value}
+    >
+      <option value="none" disabled hidden>all/some/no</option>
+      <option value="all">all</option>
+      <option value="some">some</option>
+      <option value="no">no</option>
+    </select>
+  </>
+	);
+};
+
+Quantity.propTypes = {
+	value: PropTypes.string.isRequired
+};
+
+const UserInput = (props) => {
+	// TODO: to pass down
+	const { typeOfPremise } = props;
+  return (
+    <>
+      <Quantity value="none" />
+      <Quality value="none" />
+    </>
+	);
+};
+
+UserInput.propTypes = {
+	typeOfPremise: PropTypes.string.isRequired
+};
+
+const Premise = (props) => {
   const { identity } = props;
   return (
-    <fieldset className={`part-${identity}`} />
+    <fieldset className={`part-${identity}`}>
+      <legend>{`${identity.toUpperCase().split("P")[0]}`}</legend>
+      <fieldset>
+        <legend>Choose options:</legend>
+        <UserInput />
+      </fieldset>
+      <figure>
+        <img alt="an empty image...for now" />
+        <figcaption>This will be filled...</figcaption>
+      </figure>
+    </fieldset>
   );
 };
 
-Part.propTypes = {
+Premise.propTypes = {
 	identity: PropTypes.string.isRequired
 };
 
@@ -33,7 +100,7 @@ const App = () => {
     <>
       Hello, world{foo}!
       <Form>
-        <Part identity="majorPremise" />
+        <Premise identity="majorPremise" />
       </Form>
     </>
   );
